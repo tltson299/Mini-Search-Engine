4bc8fda86a3636019850205d54abcb4993e0542b
truongson1128 <70199778+truongson1128@users.noreply.github.com>
2020-08-25 21:12:45 +0700

    Create Solution

---
+++ Mini Search Engine/function.cpp
@@ -0,0 +1,8 @@
+#include "function.h"
+
+int main()
+{
+
+
+	return 0;
+}
---
+++ Mini Search Engine/function.h
@@ -0,0 +1,24 @@
+#ifndef _FUNCTION_H_
+#define _FUNCTION_H_
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <ctime>
+#include <time.h>
+#include <algorithm>
+#include <filesystem>
+#include <sstream>
+#include <cwchar>
+#include <windows.h>
+using namespace std;
+using namespace std::filesystem;
+
+
+
+#endif
