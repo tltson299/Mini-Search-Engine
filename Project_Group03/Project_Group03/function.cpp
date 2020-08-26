9060337828cc7d97c1194503020a169fead81ba8
fizzy-1111 <luchuongtam@gmail.com>
2020-08-26 11:09:09 +0700

    insert and search function for a trie

--- Project_Group03/Project_Group03/function.cpp
+++ Project_Group03/Project_Group03/function.cpp
@@ -1,2 +1,68 @@
 #include "function.h"
+Node* Trie::createNode()
+{
+	Node* node = new Node;
+	node->isLeaf = false;
+	return node;
+}
+void Trie::insert(Node*& root, char* str, int i)
+{
+	if (root == NULL)
+		root = createNode();
+	lowerChar(str);
+	Node* cur = root;
+	while (*str)
+	{
+		lowerChar(str);
+		if (cur->mapkey.find(*str) == cur->mapkey.end())
+			cur->mapkey[*str] = createNode();
+		cur = cur->mapkey[*str];
+		str++;
+	}
+	//if(!cur->myv.empty()) cout << cur->myv.back()<<" ";
+	if (cur->myv.empty())
+		cur->myv.push_back(i);
+	if ((!cur->myv.empty()) && i > cur->myv.back())
+		cur->myv.push_back(i);
+	cur->isLeaf = true;
+}
+void Trie::deleteTrie(Node*& root)
+{
+	map<char, Node*>::iterator it;
+	if (root == NULL)
+		return;
+	for (it = root->mapkey.begin(); it != root->mapkey.end(); it++)
+		deleteTrie(it->second);
+	delete root;
+}
+// similar to search function but this function also get the leaf node
+Node* Trie::getFile(Node* root, char* str)
+{
+	lowerChar(str);
+	if (root == NULL)
+		return NULL;
+	Node* cur = root;
+	while (*str)
+	{
+		cur = cur->mapkey[*str];
+		if (cur == NULL)
+			return NULL;
+		str++;
+	}
+	return cur;
+}
+void lowerChar(char*& str)
+{
+	int n = strlen(str);
+	for (int i = 0; i < n; ++i)
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += ('a' - 'A');
+}
+void lowerString(string& str)
+{
+	int n = str.length();
+	for (int i = 0; i < n; ++i)
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += ('a' - 'A');
+}
 
--- Project_Group03/Project_Group03/function.h
+++ Project_Group03/Project_Group03/function.h
@@ -29,6 +29,16 @@ public:
 };
 
 // TREE
-
-
-#endif
+class Trie
+{
+public:
+	Node* root = nullptr;
+	Node* createNode();
+	void insert(Node*& root, char* str, int i);
+	Node* getFile(Node* root, char* str);
+	void deleteTrie(Node*& root);
+};
+// OTHER FUNCTIONS
+void lowerChar(char*& str);
+void lowerString(string& str);
+#endif  // !_FUNCTION_H_
