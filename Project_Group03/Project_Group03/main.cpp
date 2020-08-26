27ca40b193cf0b9da3f5132b93430b0550b6412c
fizzy-1111 <luchuongtam@gmail.com>
2020-08-26 15:35:44 +0700

    set up some variables

--- Project_Group03/Project_Group03/main.cpp
+++ Project_Group03/Project_Group03/main.cpp
@@ -2,7 +2,14 @@
 
 int main()
 {
-
+	// INITIALIZE VARIABLES
+	Trie trie, stopword;		// tree
+	vector<string> history;		// store history search
+	vector<string> vt1;			// store file name
+	vector<string> vt2;			// store file name
+	char* str1 = new char[50];	// store words temporarily
+	char* str2 = new char[50];	// store words temporarily
+	char* input = new char[50];
 
 	return 0;
 }
