e710dc8a8d4cffeb9c26a646e3fcffc297385364
hoangCS162 <65442689+hoangCS162@users.noreply.github.com>
2020-08-28 15:56:31 +0700

    Theme & fix gitignore

--- Project_Group03/Project_Group03/main.cpp
+++ Project_Group03/Project_Group03/main.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+	// DESIGN CONSOLE BACKGROUND
+	CONSOLE_FONT_INFOEX cfi;
+	fontSize(cfi, 0, 23);
+	// system("Color 0A"); // change background color
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+
 	// INITIALIZE VARIABLES
 	Trie trie, stopword;		// tree
 	vector<string> history;		// store history search
@@ -9,9 +15,10 @@ int main()
 	vector<string> vt2;			// store file name
 	char* str1 = new char[50];	// store words temporarily
 	char* str2 = new char[50];	// store words temporarily
-	char* input = new char[50];
+	char* input = new char[50]; // input from user
+	clock_t start = clock();	// time complexity
 
-
+	
 
 	return 0;
 }
